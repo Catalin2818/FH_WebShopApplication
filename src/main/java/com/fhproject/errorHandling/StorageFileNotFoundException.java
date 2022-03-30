7182c3c26ecfecbd07b87a73279ac12bc1349ebc
Catalin <sacadatcatalin@gmail.com>
2022-03-30 17:33:26 +0200

    storage exceptions

---
+++ src/main/java/com/fhproject/errorHandling/StorageFileNotFoundException.java
@@ -0,0 +1,11 @@
+package com.fhproject.errorHandling;
+
+public class StorageFileNotFoundException extends StorageException {
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
